21bf2fc2d73766f9c095b2817ececb51b11a53bd
Luis Alfredo López Gonzales <lalonzales@gmail.com>
2025-02-18 14:38:27 -0600

    Refactor Earth Engine token handling to write credentials in string format and update example notebook to comment out authentication step

--- .github/ee_token.py
+++ .github/ee_token.py
@@ -1,9 +1,8 @@
 import os
-import json
 
 ee_token = os.environ["EARTHENGINE_TOKEN"]
-credential = {"refresh_token": ee_token}
+credential = '{"refresh_token":"%s"}' % ee_token
 credential_file_path = os.path.expanduser("~/.config/earthengine/")
 os.makedirs(credential_file_path, exist_ok=True)
-with open(os.path.join(credential_file_path, "credentials"), "w", encoding="utf-8") as file:
-    json.dump(credential, file)
+with open(credential_file_path + "credentials", "w", encoding="utf-8") as file:
+    file.write(credential)
