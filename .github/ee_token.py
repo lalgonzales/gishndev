58e544541512271e48fb53ea32cc660d8685e520
Luis Alfredo López Gonzales <lalonzales@gmail.com>
2025-02-25 23:28:41 -0600

    Refactor Earth Engine token handling to use Google OAuth2 credentials

--- .github/ee_token.py
+++ .github/ee_token.py
@@ -1,8 +1,18 @@
+import ee
+import json
 import os
+import google.oauth2.credentials
 
-ee_token = os.environ["EARTHENGINE_TOKEN"]
-credential = '{"refresh_token":"%s"}' % ee_token
-credential_file_path = os.path.expanduser("~/.config/earthengine/")
-os.makedirs(credential_file_path, exist_ok=True)
-with open(credential_file_path + "credentials", "w", encoding="utf-8") as file:
-    file.write(credential)
+stored = json.loads(os.getenv("EARTHENGINE_TOKEN"))
+credentials = google.oauth2.credentials.Credentials(
+    None,
+    token_uri="https://oauth2.googleapis.com/token",
+    client_id=stored["client_id"],
+    client_secret=stored["client_secret"],
+    refresh_token=stored["refresh_token"],
+    quota_project_id=stored["project"],
+)
+
+ee.Initialize(credentials=credentials)
+
+print(ee.String("Greetings from the Earth Engine servers!").getInfo())
